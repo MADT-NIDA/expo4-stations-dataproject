8be0a08bbfd71958e191bfb7343d325fdf3075f7
Anthony <anthony@MacAir.local>
2025-07-23 01:30:20 +0700

    fix(compose): fix dockerbuild

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ async def read_projects(request: Request, project_slug: str):
     if project_slug == 'lift':
         payload['yt_link'] = "https://www.youtube.com/embed/-WlWMp6UCQY?si=CcaBHcnieLbG4GAV"
         payload['slide_path'] = "/static/project/lift/MADT7204_Lift.pdf"
-        payload['chat_id'] = chat_id
+        payload['chat_id'] = '41677df2-3871-4e65-bc7d-935f12dc121a'
 
     if project_slug == 'carbonix':
         payload['yt_link'] = "https://www.youtube.com/embed/UgJ3B7ADTQQ?si=Pzog1MykCi6eiUMR"
         payload['slide_path'] = "/static/project/carbonix/Carbon Xchange Pitch Deck Presentation.pdf"
-        payload['chat_id'] = chat_id
+        payload['chat_id'] = '324dde45-e851-47c1-b9f1-3ef5f9f77ab0'
 
     if project_slug == 'finsure':
         payload['yt_link'] = "https://www.youtube.com/embed/PSU_pTzm08g?si=K60ZM_6gTYdOa6k8"
@@ -59,15 +59,10 @@ async def read_projects(request: Request, project_slug: str):
     if project_slug == 'freshx':
         payload['yt_link'] = "https://www.youtube.com/embed/WeMnV-XJOQ8?si=jwxEgPWCG69kTA3V"
         payload['slide_path'] = "/static/project/carbonix/Carbon Xchange Pitch Deck Presentation.pdf"
-        payload['chat_id'] = chat_id
+        payload['chat_id'] = 'eabe90ce-b681-4bf7-a3da-0758d4515ed6'
 
     if project_slug == 'greenbridge':
         payload['yt_link'] = "https://www.youtube.com/embed/Kl6C-xaRCqo?si=LIBeQPDxvY_yfDOg"
         payload['slide_path'] = "/static/project/carbonix/Carbon Xchange Pitch Deck Presentation.pdf"
         payload['chat_id'] = chat_id
 
-
-
-
-# http://http://expo4.madt.pro:5678:5678/webhook/68282fbb-afad-437d-88ed-2c1476bb457b/chat
-    return templates.TemplateResponse("project_templates.html", payload)
